8c744818148b3bdc80e2792d61627f0a6bae580f
SailTheSeas <2430921@students.wits.ac.za>
2025-02-19 13:33:54 +0200

    Fixes

--- JavaScript/menu.js
+++ JavaScript/menu.js
@@ -8,7 +8,7 @@ const menuItems =
         //{ name: "Essays", href: `${root}/Essays/essays.html` },
         { name: "Pictures", href: `${root}/Portfolio/portfolio.html` },
         { name: "Short Films", href: `${root}/ShortFilms/portfolio.html` },
-        { name: "Games", href: `${root}/Portfolio/portfolio.html` },
+        { name: "Games", href: `${root}/Games/portfolio.html` },
         { name: "Design", href: `${root}/Designs/designs.html` },
         { name: "About", href: `${root}/About/about.html` },
     ]
